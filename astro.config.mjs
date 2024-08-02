3026a8c6438e6c681915cab1e9dbc10a2fbdd0a6
Romain <rsire@tuta.io>
2024-08-02 16:04:18 +0900

    feat(config): enable the React islands in the config files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://doglife.ovh/",
+  site: "https://doglife.ovh/",
+  integrations: [react()]
 });
